4a1fc851f7206c58b653389a2212232ce5106be7
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-02-28 15:42:22 -0300

    projeto em forma de jogo versão 1

---
+++ gamept0.js
@@ -0,0 +1,42 @@
+const { prompt } = require("./prompt");
+
+let nivel = 0
+let n = prompt("\nvc estava na cena do crime?")
+
+if(n == "s"){
+  nivel++
+}
+
+n = prompt("\nvc conhecia a vitima?")
+
+if(n == "s"){
+  nivel++
+}
+
+n = prompt("\nconhecia algum parente da vitima?")
+
+if(n == "s"){
+  nivel++
+}
+
+n = prompt("\na vitima devia algo pra ti?")
+
+if(n == "s"){
+  nivel++
+}
+
+n = prompt("\nsabe atirar?")
+
+if(n == "s"){
+  nivel++
+}
+
+console.log("\nVOCE É: \n")
+
+if(nivel >= 4){
+  console.log("aeee, culpado")
+} else if(nivel == 3){
+  console.log("HUNMMM... suspeito")
+} else{
+  console.log("...inocente, triste")
+}
